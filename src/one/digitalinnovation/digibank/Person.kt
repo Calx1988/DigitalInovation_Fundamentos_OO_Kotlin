578169d6d8c4c93bad2e66e069ce695eda6f9a2a
Cássio Alexsander <cassioalexsander@gmail.com>
2021-02-15 19:05:25 -0300

    Criada classe Person.kt

---
+++ src/one/digitalinnovation/digibank/Person.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation.digibank
+
+class Person {
+    var name:String="Cassio"
+    var surname:String="Correa"
+    var cpf:String="014.609.930.39"
+
+}
+
+fun main(){
+    val Cassio=Person()
+    println(Cassio.name)
+    println(Cassio.surname)
+    println(Cassio.cpf)
+
+}
