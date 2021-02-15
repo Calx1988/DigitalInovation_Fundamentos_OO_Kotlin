38a0bc3ce5e39be2115e905e0caa1e2967332ef3
Cássio Alexsander <cassioalexsander@gmail.com>
2021-02-15 19:16:09 -0300

    modificada classe Person.kt

--- src/one/digitalinnovation/digibank/Person.kt
+++ src/one/digitalinnovation/digibank/Person.kt
@@ -3,9 +3,7 @@ package one.digitalinnovation.digibank
 class Person {
     var name:String="Cassio"
     var surname:String="Correa"
-<<<<<<< HEAD
     var cpf:String="123.123.123.123"
->>>>>>> origin/master
 
 }
 
