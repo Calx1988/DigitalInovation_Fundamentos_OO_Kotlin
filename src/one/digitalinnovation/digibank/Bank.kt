2e48924442266ccc8b1431950cc4a04bec83f14f
Cássio Alexsander <cassioalexsander@gmail.com>
2021-02-16 19:57:18 -0300

    criada data class Bank

---
+++ src/one/digitalinnovation/digibank/Bank.kt
@@ -0,0 +1,7 @@
+package one.digitalinnovation.digibank
+
+data class Bank {
+    val name: String
+    val number: Int
+
+}
---
+++ src/one/digitalinnovation/digibank/tests/BankTest.kt
@@ -0,0 +1,7 @@
+package one.digitalinnovation.digibank.tests
+
+import one.digitalinnovation.digibank.Bank
+
+fun main(){
+    val digioneBank = Bank()
+}
