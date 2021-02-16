371d8f97f87ba759b7534bb92bdeb234a254b2af
Cássio Alexsander <cassioalexsander@gmail.com>
2021-02-16 18:26:55 -0300

    criada classe Account

---
+++ src/one/digitalinnovation/digibank/Account.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation.digibank
+
+import java.math.BigDecimal
+
+class Account {
+    val agency:String
+    val accountId: String
+    val accountBalance: BigDecimal
+
+}
+fun deposit(value:BigDecimal){
+
+}
+fun withdrawal(value:BigDecimal){
+
+}
